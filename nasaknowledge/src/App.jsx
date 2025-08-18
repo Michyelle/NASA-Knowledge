13fa809937c5b24f19b4f17009f071bb65be9d1c
michyelle <michellenguyen12@yahoo.com>
2025-08-17 23:19:18 -0700

    image of the day hero

---
+++ nasaknowledge/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Hero from './components/Hero.jsx'
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
---
+++ nasaknowledge/src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function Hero() {
+    const [media, setMedia] = useState({
+        url: '',
+        copyright: '',
+        title: '',
+        date: '',
+        explanation: '',
+    });
+
+    const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+    const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+    
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(data => {
+                setMedia({
+                    url: data.url,
+                    media_type: data.media_type,
+                    copyright: data.copyright,
+                    title: data.title,
+                    explanation: data.explanation,
+                    date: data.date,
+                });
+            })
+    }, []);
+
+
+    return (        
+        <section className="hero">
+            <h1>IMAGE OF THE DAY</h1>
+            <p>Each day a different image of our universe will be featured along with a brief explanation from NASA</p>
+            <h2>{media.title}</h2>
+            <h3>{media.date}</h3>
+
+            <div className="image-container">
+                <img width="600" height="600" src={media.url} alt="" />
+                <h3>By: {media.copyright}</h3>
+                <p>{media.explanation}</p>
+            </div>
+        </section>
+    )
+}
